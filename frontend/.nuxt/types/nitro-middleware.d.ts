56e3db9229b7e491c4fd98a07d230845e549a0ca
ihsanarsandy <ihsan.arsandy@gmail.com>
2025-12-25 10:26:16 +0700

    chore: Remove .nuxt folder from git tracking

--- frontend/.nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "admin" | "auth" | "instructor"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
